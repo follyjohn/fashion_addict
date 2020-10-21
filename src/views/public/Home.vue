2753a660935805e49ba0c38661753a5b83fa4d91
John FOLLY <follyjohn@outlook.fr>
2020-10-21 10:06:37 +0000

    start wirking

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav">
+      <nav class="navbar navbar-expand-lg navbar-light  p-4">
+        <a class="navbar-brand" href="#">fashion adict</a>
+        <button
+          type="button"
+          id="sidebarCollapse"
+          class="btn navbar-btn mr-4 active p-0"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <ul class="navbar-nav ml-auto">
+          <font-awesome-icon icon="search" />
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+#sidebarCollapse {
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+
+#sidebarCollapse:focus {
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+#sidebarCollapse span {
+  width: 80%;
+  height: 2px;
+  margin: 0 auto;
+  display: block;
+  background: #555;
+  transition: all 0.8s cubic-bezier(0.81, -0.33, 0.345, 1.375);
+  transition-delay: 0.2s;
+}
+
+#sidebarCollapse span:first-of-type {
+  transform: rotate(45deg) translate(2px, 2px);
+}
+
+#sidebarCollapse span:nth-of-type(2) {
+  opacity: 0;
+}
+
+#sidebarCollapse span:last-of-type {
+  transform: rotate(-45deg) translate(1px, -1px);
+}
+
+#sidebarCollapse.active span {
+  transform: none;
+  opacity: 1;
+  margin: 5px auto;
+}
+</style>

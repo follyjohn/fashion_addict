db2501a9effe2b333dd7a2e044f6d3f3931bdb4e
john <follyjohn@outlook.fr>
2020-10-28 16:08:46 +0000

    ok

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,56 +1,98 @@
 <template>
-  <CCardBody>
-    <CDataTable
-      :items="items"
-      :fields="fields"
-      column-filter
-      table-filter
-      items-per-page-select
-      :items-per-page="5"
-      hover
-      sorter
-      pagination
-    >
-      <template #status="{item}">
-        <td>
-          <CBadge :color="getBadge(item.status)">
-            {{ item.status }}
-          </CBadge>
-        </td>
-      </template>
-      <template #show_details="{item, index}">
-        <td class="py-2">
-          <CButton
-            color="primary"
-            variant="outline"
-            square
-            size="sm"
-            @click="toggleDetails(item, index)"
+  <div>
+    <CNavbar expandable="md" type="dark" color="info">
+      <CToggler in-navbar @click="collapsed = !collapsed" />
+      <CNavbarBrand href="#">NavBar</CNavbarBrand>
+      <CCollapse :show="collapsed" navbar>
+        <CNavbarNav>
+          <CNavItem href="#">Link</CNavItem>
+          <CNavItem href="#" disabled>Disabled</CNavItem>
+        </CNavbarNav>
+
+        <!-- Right aligned nav items -->
+        <CNavbarNav class="ml-auto">
+          <CForm inline>
+            <CInput class="mr-sm-2" placeholder="Search" size="sm" />
+            <CButton
+              color="secondary"
+              size="sm"
+              class="my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </CButton>
+          </CForm>
+
+          <CDropdown togglerText="Lang" nav placement="bottom-end">
+            <CDropdownItem>EN</CDropdownItem>
+            <CDropdownItem>ES</CDropdownItem>
+            <CDropdownItem>RU</CDropdownItem>
+            <CDropdownItem>FA</CDropdownItem>
+          </CDropdown>
+
+          <CDropdown nav togglerText="User" placement="bottom-end">
+            <CDropdownItem>Profile</CDropdownItem>
+            <CDropdownItem>Signout</CDropdownItem>
+          </CDropdown>
+        </CNavbarNav>
+      </CCollapse>
+    </CNavbar>
+    <CCardBody>
+      <CDataTable
+        :items="items"
+        :fields="fields"
+        column-filter
+        table-filter
+        items-per-page-select
+        :items-per-page="5"
+        hover
+        sorter
+        pagination
+      >
+        <template #status="{item}">
+          <td>
+            <CBadge :color="getBadge(item.status)">
+              {{ item.status }}
+            </CBadge>
+          </td>
+        </template>
+        <template #show_details="{item, index}">
+          <td class="py-2">
+            <CButton
+              color="primary"
+              variant="outline"
+              square
+              size="sm"
+              @click="toggleDetails(item, index)"
+            >
+              {{ Boolean(item._toggled) ? "Hide" : "Show" }}
+            </CButton>
+          </td>
+        </template>
+        <template #details="{item}">
+          <CCollapse
+            :show="Boolean(item._toggled)"
+            :duration="collapseDuration"
           >
-            {{ Boolean(item._toggled) ? "Hide" : "Show" }}
-          </CButton>
-        </td>
-      </template>
-      <template #details="{item}">
-        <CCollapse :show="Boolean(item._toggled)" :duration="collapseDuration">
-          <CCardBody>
-            <CMedia :aside-image-props="{ height: 102 }">
-              <h4>
-                {{ item.username }}
-              </h4>
-              <p class="text-muted">User since: {{ item.registered }}</p>
-              <CButton size="sm" color="info" class="">
-                User Settings
-              </CButton>
-              <CButton size="sm" color="danger" class="ml-1">
-                Delete
-              </CButton>
-            </CMedia>
-          </CCardBody>
-        </CCollapse>
-      </template>
-    </CDataTable>
-  </CCardBody>
+            <CCardBody>
+              <CMedia :aside-image-props="{ height: 102 }">
+                <h4>
+                  {{ item.username }}
+                </h4>
+                <p class="text-muted">User since: {{ item.registered }}</p>
+                <CButton size="sm" color="info" class="">
+                  User Settings
+                </CButton>
+                <CButton size="sm" color="danger" class="ml-1">
+                  Delete
+                </CButton>
+              </CMedia>
+            </CCardBody>
+          </CCollapse>
+        </template>
+      </CDataTable>
+    </CCardBody>
+  </div>
 </template>
 <script>
 const items = [
@@ -232,7 +274,8 @@ export default {
       }),
       fields,
       details: [],
-      collapseDuration: 0
+      collapseDuration: 0,
+      collapsed: false
     };
   },
   methods: {
